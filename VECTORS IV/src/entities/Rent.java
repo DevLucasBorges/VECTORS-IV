c0cea8bf0f5f32fbfafad09ab5e966f9c9e2c2b1
Lucas Borges <76432049+DevLucasBorges@users.noreply.github.com>
2021-05-25 19:38:12 -0300

    Add files via upload

---
+++ VECTORS IV/src/entities/Rent.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class Rent {
+
+	public Rent(String name, String email) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
